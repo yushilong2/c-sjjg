427b3c97b7938726dee72b2bfa8ef4187dbbbf2d
余世龙 <2350065497@qq.com>
2020-12-20 23:17:05 +0800

    C语言实现数据结构

--- 12_16_1/12_16_1/1.c
+++ 12_16_1/12_16_1/1.c
@@ -1,10 +1,10 @@
-//假设A，B为非空循环链表的尾指针
-linkList Connect(LinkList A, LinkList B)
-{
-	LinkList p = A->next;//保存A表的头结点位置
-	A->next = B->next->next;//B表的开始结点链接到A表尾
-	free(B->next);//释放B表的头结点
-	B->next = p;
-	return B;//返回新循环链表的尾指针
-}
-//
+/*
+* 复习前面的东西
+* int char float double
+*		数据类型 int x=0;
+* 数组  int arr[10]
+*		数据类型 用来存放多个同类型
+* 指针 *p
+*		数据类型 用来存放其他内存单元的地址
+* 结构体
+*/
